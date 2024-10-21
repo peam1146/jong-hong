1a19fa72909c96988446e17035fd0abc04ccee81
Armmmm <armernuntiya@gmail.com>
2024-10-21 23:59:18 +0700

    fix: logic auth service

--- apps-api/auth/src/app.module.ts
+++ apps-api/auth/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 import { KafkaModule } from './kafka/kafka.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './enviroment';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [RedisModule, KafkaModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(environment.MONGO_URI, {
+      user: environment.MONGO_USER,
+      pass: environment.MONGO_PASS,
+      dbName: environment.MONGO_DBNAME,
+    }),
+    RedisModule,
+    KafkaModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps-api/auth/src/auth/auth.module.ts
+++ apps-api/auth/src/auth/auth.module.ts
@@ -7,6 +7,10 @@ import { PassportModule } from '@nestjs/passport';
 import { environment } from '../enviroment';
 import { RedisService } from '../redis/redis.service';
 import { KafkaModule } from '../kafka/kafka.module';
+import { UserService } from '../user/user.service';
+import { UserModule } from '../user/user.module';
+import { RedisModule } from '../redis/redis.module';
+import { JWTAuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [
@@ -16,10 +20,11 @@ import { KafkaModule } from '../kafka/kafka.module';
         expiresIn: environment.JWT_EXPIRE_IN,
       },
     }),
+    UserModule,
     PassportModule,
     KafkaModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, RedisService],
+  providers: [AuthService, GoogleStrategy, JWTAuthGuard, RedisService],
 })
 export class AuthModule {}
