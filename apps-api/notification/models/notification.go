b0c7fc976bc17cbabeda0f0cea806dff7f58f02b
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-10-30 09:50:38 +0700

    fix: type

--- apps-api/notification/models/notification.go
+++ apps-api/notification/models/notification.go
@@ -1,8 +1,17 @@
 package models
 
+type NotificationType string
+
+const (
+	BeforeCheckin  NotificationType = "BEFORE_CHECKIN"
+	NotCheckin     NotificationType = "NOT_CHECKIN"
+	BeforeCheckout NotificationType = "BEFORE_CHECKOUT"
+	NotCheckout    NotificationType = "NOT_CHECKOUT"
+)
+
 type Notification struct {
-	Id       string `json:"id"`
-	Type     string `json:"type"`
-	UserId   string `json:"userId"`
-	CreateAt string `json:"CreateAt"`
+	Id       string           `json:"id"`
+	Type     NotificationType `json:"type"`
+	UserId   string           `json:"userId"`
+	CreateAt string           `json:"CreateAt"`
 }
