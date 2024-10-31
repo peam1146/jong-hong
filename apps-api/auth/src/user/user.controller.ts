8d18edad17fbdd2d908c5ae9665e012737bed7bc
Armmmm <armernuntiya@gmail.com>
2024-11-01 03:34:32 +0700

    clear used path

--- apps-api/auth/src/user/user.controller.ts
+++ apps-api/auth/src/user/user.controller.ts
@@ -4,16 +4,4 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Get()
-  async findAll() {
-    try {
-      const users = await this.userService.findAll();
-      return users;
-    } catch (error) {
-      throw new HttpException(
-        'Error fetching users',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
 }
